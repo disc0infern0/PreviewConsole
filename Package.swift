aa1bfc2dbe1d79338e97c3c8e8280a0d6c07dc63
Andrew <007gnail@gmail.com>
2021-11-21 22:56:57 +0000

    Removed tvOS support as drag gestures not supported

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "PreviewConsole",
-    platforms: [ .iOS(.v15), .macOS(.v12), .tvOS(.v15), .watchOS(.v8)
+    platforms: [ .iOS(.v15), .macOS(.v12), .watchOS(.v8)
                ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
